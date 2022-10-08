709cbee553c43f0db51e24a46d3af0a56ce249b0
ananthakannan92 <112980176+ananthakannan92@users.noreply.github.com>
2022-10-08 23:25:18 +0300

    Create conftest.cpp

---
+++ conftest.cpp
@@ -0,0 +1,55 @@
+/* confdefs.h */
+#define PACKAGE_NAME "Virtualempire Core"
+#define PACKAGE_TARNAME "virtualempire"
+#define PACKAGE_VERSION "1.0.0"
+#define PACKAGE_STRING "Virtualempire Core 1.0.0"
+#define PACKAGE_BUGREPORT "https://github.com/ananthakannan92/Virtualempire/issues"
+#define PACKAGE_URL "https://virtualempire.org"
+#define HAVE_CXX11 1
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define LT_OBJDIR ".libs/"
+#define USE_ASM 1
+#define HAVE_LIBMINGWTHRD 1
+#define HAVE_LIBKERNEL32 1
+#define HAVE_LIBUSER32 1
+#define HAVE_LIBGDI32 1
+#define HAVE_LIBCOMDLG32 1
+#define HAVE_LIBWINSPOOL 1
+#define HAVE_LIBWINMM 1
+#define HAVE_LIBSHELL32 1
+#define HAVE_LIBCOMCTL32 1
+#define HAVE_LIBOLE32 1
+#define HAVE_LIBOLEAUT32 1
+#define HAVE_LIBUUID 1
+#define HAVE_LIBRPCRT4 1
+#define HAVE_LIBADVAPI32 1
+#define HAVE_LIBWS2_32 1
+#define HAVE_LIBMSWSOCK 1
+#define HAVE_LIBSHLWAPI 1
+#define HAVE_LIBIPHLPAPI 1
+#define HAVE_LIBCRYPT32 1
+#define HAVE_PTHREAD_PRIO_INHERIT 1
+#define HAVE_PTHREAD 1
+#define _FILE_OFFSET_BITS 64
+#define HAVE_DECL_STRERROR_R 0
+#define HAVE_FUNC_ATTRIBUTE_VISIBILITY 1
+#define HAVE_FUNC_ATTRIBUTE_DLLEXPORT 1
+#define HAVE_FUNC_ATTRIBUTE_DLLIMPORT 1
+/* end confdefs.h.  */
+
+int
+main ()
+{
+
+  ;
+  return 0;
+}
