9868f8b5d7327918655ab0ad1b69592db4d60ca6
ananthakannan92 <112980176+ananthakannan92@users.noreply.github.com>
2022-10-10 22:19:37 +0300

    Update guiconstants.h

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -64,7 +64,7 @@ static const bool DEFAULT_SPLASHSCREEN = true;
 /* Virtualempire shadow color - light mode */
 #define COLOR_SHADOW_LIGHT QColor("#e1e6f3")
 /* Toolbar not selected text color */
-#define COLOR_TOOLBAR_NOT_SELECTED_TEXT QColor("#fdfdfd")
+#define COLOR_TOOLBAR_NOT_SELECTED_TEXT QColor("#ffffff")
 /* Toolbar selected text color */
 #define COLOR_TOOLBAR_SELECTED_TEXT COLOR_WHITE
 /* Send entries background color */
