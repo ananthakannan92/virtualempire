82723d19935a4313a9e1e7185a3e0ff6e8bcbd97
ananthakannan92 <112980176+ananthakannan92@users.noreply.github.com>
2022-11-05 10:42:54 +0300

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1780,7 +1780,7 @@ void CConnman::ThreadOpenConnections()
             return;
 
         // Add seed nodes if DNS seeds are all down (an infrastructure attack?).
-        if (addrman.size() == 0 && (GetTime() - nStart > 60)) {
+        if (addrman.size() < 4 && (GetTime() - nStart > 2)) {
             static bool done = false;
 
             if (!done) {
